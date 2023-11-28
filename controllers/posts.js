60955cb4d0102288729a5e91ca9c3e8a3f6f724d
Rahul Kumar <username78588@gmail.com>
2023-11-28 22:45:29 +0530

    ADD:CreateComment Endpoints

--- controllers/posts.js
+++ controllers/posts.js
@@ -5,26 +5,25 @@ import { v2 as cloudinary } from "cloudinary";
 export const createPost = async (req, res) => {
   try {
     const { userId, description, picturePath } = req.body;
-  // console.log("Req File",req.file);
+    // console.log("Req File",req.file);
 
-        const user = await User.findById(userId);
-        const newPost = new Post({
-          userId,
-          firstName: user.firstName,
-          lastName: user.lastName,
-          location: user.location,
-          description,
-          userPicturePath: user.picturePath,
-          picturePath: req.file.filename,
-          likes: {},
-          comments: [],
-        });
-        await newPost.save();
-  
-        const post = await Post.find();
-        res.status(201).json(post);
-        res.status(200).json({ success: true });
-      
+    const user = await User.findById(userId);
+    const newPost = new Post({
+      userId,
+      firstName: user.firstName,
+      lastName: user.lastName,
+      location: user.location,
+      description,
+      userPicturePath: user.picturePath,
+      picturePath: req.file.filename,
+      likes: {},
+      comments: [],
+    });
+    await newPost.save();
+
+    const post = await Post.find();
+    res.status(201).json(post);
+    res.status(200).json({ success: true });
   } catch (err) {
     res.status(409).json({ message: err.message });
   }
@@ -51,6 +50,39 @@ export const getUserPosts = async (req, res) => {
 };
 
 /* UPDATE */
+export const commentPost = async (req, res) => {
+  const { id } = req.params;
+  const { content } = req.body;
+
+  try {
+    // Find the post by postId
+    //console.log("postId is ", id);
+    const post = await Post.findById(id);
+    //console.log("post is ", post);
+    if (!post) {
+      //console.log("error at 63");
+      return res.status(404).json({ message: "Post not found" });
+    }
+    //console.log("Coming");
+    // Find the comment in the post's comments array
+    //const comment = post.comments.find((c) => c._id.toString() === commentId);
+    post.comments.push({ content: content });
+    // if (comment) {
+    //   console.log("error at 71");
+    //   return res.status(404).json({ message: 'Comment not found' });
+    // }
+
+    // // Update the comment content
+    // comment.content = updatedComment;
+
+    // Save the updated post
+    await post.save();
+    res.status(200).json({ message: "Comment updated successfully", post });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Internal server error" });
+  }
+};
 export const likePost = async (req, res) => {
   try {
     const { id } = req.params;
